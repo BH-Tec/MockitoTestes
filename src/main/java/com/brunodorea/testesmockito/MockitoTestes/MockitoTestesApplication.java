48a69d40a8b1b830bb51d14dcac7655205b4c548
Bruno Henrique Félix Dórea <brunodorea@outlook.com.br>
2022-08-10 10:56:55 -0300

    Initial commit

---
+++ src/main/java/com/brunodorea/testesmockito/MockitoTestes/MockitoTestesApplication.java
@@ -0,0 +1,13 @@
+package com.brunodorea.testesmockito.MockitoTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoTestesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunodorea/testesmockito/MockitoTestes/MockitoTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunodorea.testesmockito.MockitoTestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
